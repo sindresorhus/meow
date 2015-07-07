56b957fb0d60bd791a06f1862d322f8912661af4
Kevin Martensson <kevinmartensson@gmail.com>
2015-07-07 11:55:13 +0200

    Add more tests
    Fixes #5.

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
 'use strict';
+var childProcess = require('child_process');
 var test = require('ava');
 var indentString = require('indent-string');
 var meow = require('./');
 
 test('return object', function (t) {
-	var cli = meow({argv: ['foo', '--foo-bar']});
+	var cli = meow({
+		argv: ['foo', '--foo-bar', '-u', 'cat'],
+		help: [
+			'Usage',
+			'  foo <input>'
+		]
+	}, {
+		alias: {u: 'unicorn'},
+		default: {meow: 'dog'}
+	});
+
 	t.assert(cli.input[0] === 'foo');
 	t.assert(cli.flags.fooBar);
+	t.assert(cli.flags.meow === 'dog');
+	t.assert(cli.flags.unicorn === 'cat');
 	t.assert(cli.pkg.name === 'meow');
+	t.assert(cli.help === indentString('\nCLI app helper\n\nUsage\n  foo <input>', '  '));
 	t.end();
 });
 
@@ -15,4 +29,31 @@ test('support help shortcut', function (t) {
 	var cli = meow(['unicorn', 'cat']);
 	t.assert(cli.help === indentString('\nCLI app helper\n\nunicorn\ncat', '  '));
 	t.end();
+});
+
+test('spawn cli and show version', function (t) {
+	t.plan(2);
+
+	childProcess.execFile('./fixture.js', ['--version'], {cwd: __dirname}, function (err, stdout) {
+		t.assert(!err, err);
+		t.assert(stdout.trim() === require('./package.json').version);
+	});
+});
+
+test('spawn cli and show help screen', function (t) {
+	t.plan(2);
+
+	childProcess.execFile('./fixture.js', ['--help'], {cwd: __dirname}, function (err, stdout) {
+		t.assert(!err, err);
+		t.assert(stdout === indentString('\nCLI app helper\n\nUsage\n  foo <input>\n', '  '));
+	});
+});
+
+test('spawn cli and test input', function (t) {
+	t.plan(2);
+
+	childProcess.execFile('./fixture.js', ['-u', 'cat'], {cwd: __dirname}, function (err, stdout) {
+		t.assert(!err, err);
+		t.assert(stdout === 'u\nunicorn\nmeow\n');
+	});
 });
