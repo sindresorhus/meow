3e05a2ee38559b848b487d3fae892b271dfb5908
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-11-18 08:08:23 +0100

    Add type information for flags (#122)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,47 +1,51 @@
-import {expectType} from 'tsd';
+import {expectAssignable, expectType} from 'tsd';
 import {PackageJson} from 'type-fest';
 import meow = require('.');
 import {Result} from '.';
 
-expectType<Result>(meow('Help text'));
-expectType<Result>(meow('Help text', {hardRejection: false}));
-expectType<Result>(
-	meow({
-		flags: {
-			unicorn: {
-				type: 'boolean',
-				alias: 'u'
-			},
-			fooBar: {
-				type: 'string',
-				default: 'foo'
-			}
-		}
-	})
+expectType<Result<never>>(meow('Help text'));
+expectType<Result<never>>(meow('Help text', {hardRejection: false}));
+expectAssignable<{flags: {foo: number}}>(
+	meow({flags: {foo: {type: 'number'}}})
 );
-expectType<Result>(meow({description: 'foo'}));
-expectType<Result>(meow({description: false}));
-expectType<Result>(meow({help: 'foo'}));
-expectType<Result>(meow({help: false}));
-expectType<Result>(meow({version: 'foo'}));
-expectType<Result>(meow({version: false}));
-expectType<Result>(meow({autoHelp: false}));
-expectType<Result>(meow({autoVersion: false}));
-expectType<Result>(meow({pkg: {foo: 'bar'}}));
-expectType<Result>(meow({argv: ['foo', 'bar']}));
-expectType<Result>(meow({inferType: true}));
-expectType<Result>(meow({booleanDefault: true}));
-expectType<Result>(meow({booleanDefault: null}));
-expectType<Result>(meow({booleanDefault: undefined}));
-expectType<Result>(meow({hardRejection: false}));
+expectAssignable<{flags: {foo: string}}>(
+	meow({flags: {foo: {type: 'string'}}})
+);
+expectAssignable<{flags: {foo: boolean}}>(
+	meow({flags: {foo: {type: 'boolean'}}})
+);
+expectType<Result<never>>(meow({description: 'foo'}));
+expectType<Result<never>>(meow({description: false}));
+expectType<Result<never>>(meow({help: 'foo'}));
+expectType<Result<never>>(meow({help: false}));
+expectType<Result<never>>(meow({version: 'foo'}));
+expectType<Result<never>>(meow({version: false}));
+expectType<Result<never>>(meow({autoHelp: false}));
+expectType<Result<never>>(meow({autoVersion: false}));
+expectType<Result<never>>(meow({pkg: {foo: 'bar'}}));
+expectType<Result<never>>(meow({argv: ['foo', 'bar']}));
+expectType<Result<never>>(meow({inferType: true}));
+expectType<Result<never>>(meow({booleanDefault: true}));
+expectType<Result<never>>(meow({booleanDefault: null}));
+expectType<Result<never>>(meow({booleanDefault: undefined}));
+expectType<Result<never>>(meow({hardRejection: false}));
 
-const result = meow('Help text');
+const result = meow('Help text', {
+	flags: {
+		foo: {type: 'boolean', alias: 'f'},
+		'foo-bar': {type: 'number'}
+	}}
+);
 
 expectType<string[]>(result.input);
-expectType<{[name: string]: unknown}>(result.flags);
-expectType<{[name: string]: unknown}>(result.unnormalizedFlags);
 expectType<PackageJson>(result.pkg);
 expectType<string>(result.help);
+
+expectType<boolean>(result.flags.foo);
+expectType<boolean>(result.unnormalizedFlags.foo);
+expectType<unknown>(result.unnormalizedFlags.f);
+expectType<number>(result.unnormalizedFlags['foo-bar']);
+expectType<unknown>(result.flags.fooBar);
 
 result.showHelp();
 result.showHelp(1);
