1fa4b0b6b5c109e27d1f58456e79ff959d1b8e89
Oskar Löfgren <islander.abroad@gmail.com>
2020-04-15 14:10:36 +0200

    :pencil2: camelCase `minimistoptions`

--- index.js
+++ index.js
@@ -51,28 +51,28 @@ const meow = (helpText, options) => {
 		options.flags
 	) : options.flags;
 
-	let minimistoptions = {
+	let minimistOptions = {
 		arguments: options.input,
 		...minimistFlags
 	};
 
-	minimistoptions = decamelizeKeys(minimistoptions, '-', {exclude: ['stopEarly', '--']});
+	minimistOptions = decamelizeKeys(minimistOptions, '-', {exclude: ['stopEarly', '--']});
 
 	if (options.inferType) {
-		delete minimistoptions.arguments;
+		delete minimistOptions.arguments;
 	}
 
-	minimistoptions = buildMinimistOptions(minimistoptions);
+	minimistOptions = buildMinimistOptions(minimistOptions);
 
-	if (minimistoptions['--']) {
-		minimistoptions.configuration = {
-			...minimistoptions.configuration,
+	if (minimistOptions['--']) {
+		minimistOptions.configuration = {
+			...minimistOptions.configuration,
 			'populate--': true
 		};
 	}
 
 	const {pkg} = options;
-	const argv = yargs(options.argv, minimistoptions);
+	const argv = yargs(options.argv, minimistOptions);
 	let help = redent(trimNewlines((options.help || '').replace(/\t+\n*$/, '')), 2);
 
 	normalizePackageData(pkg);
