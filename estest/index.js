c16dc55fc9ab505296da2abde0358b351f415064
jimthedev <jimthedev@gmail.com>
2020-04-27 11:30:40 -0500

    fix: enable es modules

---
+++ estest/index.js
@@ -0,0 +1,25 @@
+import { createRequire } from "module";
+
+const meow = createRequire(import.meta.url)("../index.js");
+
+meow(
+	`
+Usage
+  $ estest <input>
+
+Options
+  --rainbow, -r  Include a rainbow
+
+Examples
+  $ estest unicorns --rainbow
+  🌈 unicorns 🌈
+`,
+	{
+		flags: {
+			rainbow: {
+				type: "boolean",
+				alias: "r"
+			}
+		}
+	}
+);
