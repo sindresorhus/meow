fa2a3746acba0d68297685066ec665b88671db31
Sindre Sorhus <sindresorhus@gmail.com>
2020-07-07 00:28:58 +0800

    Fix typo
    Closes #153

--- index.d.ts
+++ index.d.ts
@@ -37,7 +37,7 @@ declare namespace meow {
 		- `alias`: Usually used to define a short flag alias.
 		- `default`: Default value when the flag is not specified.
 		- `isRequired`: Determine if the flag is required.
-			If it's only known at runtime whether the flag is requried or not you can pass a Function instead of a boolean, which based on the given flags and other non-flag arguments should decide if the flag is required.
+			If it's only known at runtime whether the flag is required or not you can pass a Function instead of a boolean, which based on the given flags and other non-flag arguments should decide if the flag is required.
 		- `isMultiple`: Indicates a flag can be set multiple times. Values are turned into an array. (Default: false)
 
 		@example
