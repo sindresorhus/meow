ce2954bd2d8b8489011ca0ee54c3cf9424f5b0bb
tommy-mitchell <tmitchell7@uh.edu>
2023-08-01 12:58:29 -0500

    chore(`rollup`): use `terser`, handle licenses

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import license from 'rollup-plugin-license';
+import terser from '@rollup/plugin-terser';
+import filesize from 'rollup-plugin-filesize';
 import {defineConfig} from 'rollup';
 
 export default defineConfig({
 	input: 'source/index.js',
 	output: {
 		file: 'dist/index.js',
+		// https://rollupjs.org/configuration-options/#output-interop
 		interop: 'esModule',
+		// https://rollupjs.org/configuration-options/#output-generatedcode
 		generatedCode: {
 			preset: 'es2015',
 		},
@@ -16,5 +21,18 @@ export default defineConfig({
 		json(),
 		commonjs(),
 		nodeResolve(),
+		license({
+			thirdParty: {
+				output: 'dist/dependencies.md',
+			},
+		}),
+		terser({
+			module: true,
+			toplevel: true,
+			format: {
+				comments: false,
+			},
+		}),
+		filesize(),
 	],
 });
