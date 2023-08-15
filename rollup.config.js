485ffad1a737925dcaa37e6cb47db62ecf76361f
tommy-mitchell <tmitchell7@uh.edu>
2023-08-15 10:58:42 -0500

    fix removing comments from deps bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,16 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import license from 'rollup-plugin-license';
-/// import strip from 'strip-comments';
+import {createTag, replaceResultTransformer} from 'common-tags';
+import {delete_comments as deleteComments} from 'delete_comments';
 import {defineConfig} from 'rollup';
+
+const stripComments = createTag(
+	{onEndResult: deleteComments},
+	// Remove empty lines
+	// https://stackoverflow.com/q/16369642/10292952
+	replaceResultTransformer(/^\s*[\r\n]/gm, ''),
+);
 
 const outputDirectory = 'build';
 
@@ -28,13 +36,12 @@ export default defineConfig({
 			}
 		},
 		hoistTransitiveImports: false,
-		// TODO: strip deletes `https://...`
-		// plugins: [{
-		// 	name: 'strip-dependency-comments',
-		// 	renderChunk(code, chunk) {
-		// 		return chunk.name === 'dependencies' ? strip(code) : null;
-		// 	},
-		// }],
+		plugins: [{
+			name: 'strip-dependency-comments',
+			renderChunk(code, chunk) {
+				return chunk.name === 'dependencies' ? stripComments(code) : null;
+			},
+		}],
 	},
 	treeshake: {
 		moduleSideEffects: 'no-external',
