9961bc0395b3c2b4ec176773a3adadf6971d9aa9
Tommy <tmitchell7@uh.edu>
2024-02-28 23:43:46 -0600

    Make options required (#260)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test/options/import-meta.js
+++ test/options/import-meta.js
@@ -1,24 +1,45 @@
 import test from 'ava';
 import meow from '../../source/index.js';
 
-test('main', t => {
-	t.notThrows(() => meow({importMeta: import.meta}));
+const verifyImportMeta = test.macro((t, {cli, error}) => {
+	if (error) {
+		t.throws(cli, {message: 'The `importMeta` option is required. Its value must be `import.meta`.'});
+	} else {
+		t.notThrows(cli);
+	}
 });
 
-test('with help shortcut', t => {
-	t.notThrows(() => (
-		meow(`
-			unicorns
-			rainbows
-		`, {
-			importMeta: import.meta,
-		})
-	));
+test('main', verifyImportMeta, {
+	cli: () => meow({
+		importMeta: import.meta,
+	}),
 });
 
-test('invalid package url', t => {
-	t.throws(
-		() => meow({importMeta: '/path/to/package'}),
-		{message: 'The `importMeta` option is required. Its value must be `import.meta`.'},
-	);
+test('with help shortcut', verifyImportMeta, {
+	cli: () => meow(`
+		unicorns
+		rainbows
+	`, {
+		importMeta: import.meta,
+	}),
+});
+
+test('invalid package url', verifyImportMeta, {
+	cli: () => meow({importMeta: '/path/to/package'}),
+	error: true,
+});
+
+test('throws if unset', verifyImportMeta, {
+	cli: () => meow('foo', {}),
+	error: true,
+});
+
+test('throws if unset - options only', verifyImportMeta, {
+	cli: () => meow({}),
+	error: true,
+});
+
+test('throws if unset - help shortcut only', verifyImportMeta, {
+	cli: () => meow('foo'),
+	error: true,
 });
